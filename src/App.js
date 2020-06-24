f67cad8cf762d23bd1ffa3352fc00b0ed6b146e4
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-06-24 20:42:14 +0100

    Second challenge finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+      api.get('/repositories').then(response => {
+          console.log(response);
+          setProjects(response.data);
+      })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      url: "http://test.test",
+      title: `Challenge React Front-End ${Date.now()}`,
+      techs: [ "NodeJS", "ReactJS", "Express" ]
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
+    const response = await api.delete(`repositories/${id}`);
+
+    if(response.status == 204)
+    {
+      setProjects(projects.filter(p => p.id !== id));
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+        {projects.map(project => <li key={project.id}>{project.title}
+          <button onClick={() => handleRemoveRepository(project.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
